fb51a28fd8c2ea6d7a838392547651c8aefdddf9
Jose Florido <mn17jilf@leeds.ac.uk>
2023-04-04 16:51:12 +0100

    Looked at snippets of code; env not there anymore

--- src/data_visualisation/view_test_dat.py
+++ src/data_visualisation/view_test_dat.py
@@ -2,60 +2,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
 from mpl_toolkits.mplot3d import Axes3D
 
+# Import Data For Plot 1 and 2 (The scatter graph)
 fname_test = "results/raw/uniform/test.dat"
 train_state = np.loadtxt(fname_test, delimiter=" ", skiprows=1)
-# data = train_state[:, 2]
-# x = train_state[:, 0]
-# y = train_state[:, 0]
 
-# X, Y = np.meshgrid(x, y)
+# Import Data For Plot3 (2D Contour)
+fname_regular = ""
+reg_stuff = np.loadtxt(fname_regular, delimiter=" ")
 
+# Plot 1 (Distribution of points)
+plt.figure(1)
+ax = plt.axes()
+ax.scatter(
+    train_state[:, 0], train_state[:, 1], marker=".",
+)
+ax.set_xlabel("$x$")
+ax.set_ylabel("$t$")
 
-# print(np.shape(X))
-
-
-plt.figure()
+# Plot 2 (Coloured 3D Scatter graph) #
+plt.figure(2)
 ax = plt.axes(projection=Axes3D.name)
 ax.scatter3D(
     train_state[:, 0],
     train_state[:, 1],
     train_state[:, 2],
-    ".",
+    c=train_state[:, 2],
+    marker=".",
+    s=20,
+    cmap="coolwarm",
 )
+ax.set_xlabel("$x$")
+ax.set_ylabel("$t$")
+ax.set_title("$u(x,t)$")
 plt.show()
-# Z = pd.pivot_table(train_state, index="x", columns="y", values="z").T.values
-# X_unique = np.sort(train_state.x.unique())
-# Y_unique = np.sort(train_state.y.unique())
-# X, Y = np.meshgrid(X_unique, Y_unique)
-# print(X_unique.shape)
-# print(Y_unique.shape)
 
-# df = pd.DataFrame(dict(x=train_state[:, 0], t=train_state[:, 1], u=train_state[:, 2]))
-# xcol, tcol, ucol = "x", "t", "u"
-# df = df.sort_values(by=[xcol, tcol])
-# print(train_state.shape)
-# xvals = df[xcol].unique()
-# print(xvals.shape)
-# print(train_state[:, 0].shape)
-# tvals = df[tcol].unique()
-# print(tvals.shape)
-# print(train_state[:, 1].shape)
-# uvals = df[ucol].values.reshape(len(xvals), len(tvals))
-
-# plt.figure()
-# ax = plt.axes()
-# ax = plt.plot(train_state[:, 0], train_state[:, 1], ".")
-# # ax.pcolormesh(
-# #     tvals,
-# #     xvals,
-# #     uvals,
-# #     cmap="rainbow",
-# # # )
-# # ax.set_xlabel("$t$")
-# # ax.set_ylabel("$x$")
-# # ax.set_title("$u(x,t)$")  # ("$y_{}$".format(i + 1))
-
-# plt.show()
