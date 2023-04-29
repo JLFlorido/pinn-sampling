f07eb22e8123deacd02553ce2db0f3de2705161d
Jose Florido <mn17jilf@leeds.ac.uk>
2023-04-29 19:01:05 +0100

    codes for looking at loss done, pending scatter

--- src/data_visualisation/view_lossdat.py
+++ src/data_visualisation/view_lossdat.py
@@ -5,81 +5,57 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
 # Import Loss Data
-fname_loss1 = "results/raw/loss_RAR-D_2000_100rounds.dat"
-fname_loss2 = "results/raw/loss_RAR-D_2000_Try2.dat"
-fname_loss3 = "results/raw/loss_RAR-D_2000_a.dat"
-loss_100rounds_bigAdam = np.loadtxt(fname_loss1, delimiter=" ", skiprows=1)
-loss_10rounds_smallAdam = np.loadtxt(fname_loss2, delimiter=" ", skiprows=1)
-loss_10rounds_bigAdam = np.loadtxt(fname_loss3, delimiter=" ", skiprows=1)
+fname_loss1 = "results/raw/loss/loss_RAD_default.dat"
+fname_loss2 = "results/raw/loss/loss_RAD_RandomInit.dat"
+fname_loss3 = "results/raw/loss/loss_RAD_2kSamplePeriod.dat"
+loss_1 = np.loadtxt(fname_loss1, delimiter=" ", skiprows=1)
+loss_2 = np.loadtxt(fname_loss2, delimiter=" ", skiprows=1)
+loss_3 = np.loadtxt(fname_loss3, delimiter=" ", skiprows=1)
 
-# Data for loss and l2 over iterations
-fname_rad_loss = "results/raw/loss_RAD_default.dat"
-loss_rad = np.loadtxt(fname_rad_loss, delimiter=" ", skiprows=1)
-fname_uni_loss = "results/raw/loss_Ham_noidea.dat"
-loss_uniform = np.loadtxt(fname_uni_loss, delimiter=" ", skiprows=1)
+# More imports if necessary
+# fname_loss4 = "results/raw/.dat"
+# loss_4 = np.loadtxt(fname_loss4, delimiter=" ", skiprows=1)
+# fname_loss5 = "results/raw/.dat"
+# loss_5 = np.loadtxt(fname_loss5, delimiter=" ", skiprows=1)
 
 # ------------------------------------------------------- Plot 1 --------------------------------------------------------
-plt.figure(0)
+plt.figure(1)
 ax = plt.axes()
-ax.plot(loss_uniform[:, 0], loss_uniform[:, 1], label="Uniform")
-ax.plot(loss_rad[:, 0], loss_rad[:, 2], label="RAD Test")
-ax.plot(
-    loss_10rounds_bigAdam[:, 0],
-    loss_10rounds_bigAdam[:, 2],
-    label="RAR-D, 10 Re-Samples, 15k Adam",
-)
-ax.plot(
-    loss_100rounds_bigAdam[:, 0],
-    loss_100rounds_bigAdam[:, 2],
-    label="RAR-D, 100 Re-Samples, 15k Adam",
-)
+ax.plot(loss_1[:, 0], loss_1[:, 2], label="Default RAD, 100 Re-samples")
+ax.plot(loss_2[:, 0], loss_2[:, 2], label="1k LBFG-S limit, 10 Re-Samples")
+ax.plot(loss_3[:, 0], loss_3[:, 2], label="2k LBFG-S limit, 10 Re-Samples")
+
 
 ax.set_yscale("log")
 ax.set_xlabel("Steps")
 ax.set_ylabel("Loss")
-ax.set_title("Comparing all methods so far")
+ax.set_title("Comparing Test Loss across RAD cases")
 ax.legend()
 
 # ------------------------------------------------------- Plot 2 --------------------------------------------------------
 plt.figure(2)
 ax = plt.axes()
-ax.plot(loss_uniform[:, 0], loss_uniform[:, 1], label="Uniform")
-ax.plot(loss_rad[:, 0], loss_rad[:, 2], label="RAD Test")
-ax.plot(
-    loss_10rounds_bigAdam[:, 0],
-    loss_10rounds_bigAdam[:, 2],
-    label="RAR-D, 10 Re-Samples, 15k Adam",
-)
-ax.plot(
-    loss_100rounds_bigAdam[:, 0],
-    loss_100rounds_bigAdam[:, 2],
-    label="RAR-D, 100 Re-Samples, 15k Adam",
-)
+ax.plot(loss_1[:, 0], loss_1[:, 2], label="Default RAD, 100 Re-samples")
+ax.plot(loss_2[:, 0], loss_2[:, 2], label="1k LBFG-S limit, 10 Re-Samples")
+ax.plot(loss_3[:, 0], loss_3[:, 2], label="2k LBFG-S limit, 10 Re-Samples")
 
 ax.set_yscale("log")
 ax.set_xlim(0, 50000)
 ax.set_xlabel("Steps")
 ax.set_ylabel("Loss")
-ax.set_title("Comparing all methods so far")
+ax.set_title("Comparing Test Loss across RAD cases")
 ax.legend()
 
 # ------------------------------------------------------- Plot 3 --------------------------------------------------------
-plt.figure(1)
-ax = plt.axes()
-ax.plot(
-    loss_10rounds_smallAdam[:, 0],
-    loss_10rounds_smallAdam[:, 2],
-    label="1 500 Adam Steps",
-)
-ax.plot(
-    loss_10rounds_bigAdam[:, 0], loss_10rounds_bigAdam[:, 2], label="15 000 Adam Steps"
-)
-# ax.plot(loss_100rounds_bigAdam[:, 0], loss_100rounds_bigAdam[:, 2], label="100 Big")
+# plt.figure(3)
+# ax = plt.axes()
+# ax.plot(loss_1[:, 0], loss_1[:, 2], label="")
+# ax.plot(loss_2[:, 0], loss_2[:, 2], label="")
 
-ax.set_yscale("log")
-ax.set_xlabel("Steps")
-ax.set_ylabel("Loss")
-ax.set_title("Comparing RAR-D settings via test loss")
-ax.legend()
+# ax.set_yscale("log")
+# ax.set_xlabel("Steps")
+# ax.set_ylabel("Loss")
+# ax.set_title("")
+# ax.legend()
 
 plt.show()
--- src/data_visualisation/view_traindat.py
+++ src/data_visualisation/view_traindat.py
@@ -6,11 +6,9 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Import Data For Plot 1 and 2 (The scatter graph)
 fname_test = "results/raw/test_uniform.dat"
+test_state = np.loadtxt(fname_test, delimiter=" ", skiprows=1)
+fname_train = "results/raw/train_uniform.dat"
 train_state = np.loadtxt(fname_test, delimiter=" ", skiprows=1)
-
-# # Import Data For Plot3 (2D Contour)
-# fname_regular = ""
-# reg_stuff = np.loadtxt(fname_regular, delimiter=" ")
 
 # Plot 1 (Distribution of points)
 plt.figure(1)
@@ -38,38 +36,5 @@ ax.scatter3D(
 ax.set_xlabel("$x$")
 ax.set_ylabel("$t$")
 ax.set_title("$u(x,t)$")
-
-# Data for loss and l2 over iterations
-fname_traind = "results/raw/train_RAD_default.dat"
-train_state = np.loadtxt(fname_traind, delimiter=" ", skiprows=1)
-fname_rad_loss = "results/raw/loss_RAD_default.dat"
-loss_rad = np.loadtxt(fname_rad_loss, delimiter=" ", skiprows=1)
-fname_uni_loss = "results/raw/loss_uniform.dat"
-loss_uniform = np.loadtxt(fname_uni_loss, delimiter=" ", skiprows=1)
-plt.figure(3)
-ax = plt.axes()
-ax.plot(
-    loss_rad[:, 0],
-    loss_rad[:, 1],
-    loss_rad[:, 0],
-    loss_rad[:, 2],
-    loss_uniform[:, 0],
-    loss_uniform[:, 1],
-    loss_uniform[:, 0],
-    loss_uniform[:, 2],
-)
-ax.set_yscale("log")
-plt.figure(4)
-ax = plt.axes()
-ax.plot(
-    loss_uniform[:, 0],
-    loss_uniform[:, 1],
-    loss_uniform[:, 0],
-    loss_uniform[:, 2],
-)
-ax.set_yscale("log")
-# ax.plot()
-
-# plt.figure(4)
 
 plt.show()
