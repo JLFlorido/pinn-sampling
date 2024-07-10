b78d96ae48e68790783d328490df83ebc4a1d523
Jose Florido <mn17jilf@leeds.ac.uk>
2024-07-10 14:55:07 +0100

    Working code but needs some qol and memory improvements

--- src/burgers_2d/be2d_fixed_arc.py
+++ src/burgers_2d/be2d_fixed_arc.py
@@ -1,5 +1,5 @@
-"""Run PINN to solve Burger's Equation in 2D using adaptive resampling, adapted to run in ARC (HPC).
-be2d_arc.py. 
+"""Run using fixed distribution of points without resampling.
+be2d_fixed_arc.py. 
 
 Usage:
     be2d_arc.py [--k=<hyp_k>] [--c=<hyp_c>] [--N=<NumDomain>] [--L=<NumResamples> ] [--IM=<InitialMethod>] [--DEP=<depth>] [--INP1=<input1>]
@@ -12,7 +12,7 @@ Options:
     --L=<NumResamples>          Number of times points are resampled [default: 100]
     --IM=<InitialMethod>        Initial distribution method from: "Grid","Random","LHS", "Halton", "Hammersley", "Sobol" [default: Random]
     --DEP=<depth>               Depth of the network [default: 3]
-    --INP1=<input1>             Info source, "uxt", "uxut1" etc... [default: residual]
+    --INP1=<input1>             Info source, "uxt", "uxut1" etc... [default: fixed]
 """
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Initial imports and some function definitions.
@@ -26,11 +26,9 @@ import deepxde as dde
 from deepxde.backend import tf
 import numpy as np
 import time
-# import matplotlib.pyplot as plt
 
-# os.environ['DDE_BACKEND'] = 'tensorflow.compat.v1'
 # dde.config.set_default_float("float64")
-dde.optimizers.config.set_LBFGS_options(maxiter=1000)
+dde.optimizers.config.set_LBFGS_options(maxiter=201000)
 
 def gen_testdata(): # This function opens the ground truth solution at a fixed uniform set of points.
     X = np.load("./npy_files/X.npy")
@@ -72,13 +70,13 @@ def quasirandom(n_samples, sampler): # This function creates pseudorandom distri
 # Main code start
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def main(k=1, c=1, NumDomain=2000, NumResamples=100, method="Random", depth=3, input1="residual"): # Main Code
+def main(k=1, c=1, NumDomain=2000, NumResamples=100, method="Random", depth=3, input1="none"): # Main Code
     print(f"k equals {k}")
     print(f"c equals {c}")
     print(f"NumDomain equals {NumDomain}")
     print(f"Method equals {method}")
     print(f"Depth equals {depth}")
-    print(f"Input1 equals {input1}")
+    print(f"Input1 not needed")
     start_t = time.time() #Start time.
 
     # Defining PDE and all derivatives of u, v, and PDE used for guiding resampling.
@@ -98,61 +96,6 @@ def main(k=1, c=1, NumDomain=2000, NumResamples=100, method="Random", depth=3, i
         dv_yy = dde.grad.hessian(u, x, component=1, i=1, j=1)
         pde_v = dv_t + (u_vel * dv_x) + (v_vel * dv_y) - (0.01 / np.pi * dv_xx) - (0.01 / np.pi * dv_yy)
         return [pde_u, pde_v]
-        
-    def u_xy(x, u):
-        u_vel = u[:, 0:1]
-        return dde.grad.hessian(u_vel, x, i=0, j=1)
-    def u_xt(x, u):
-        u_vel = u[:, 0:1]
-        return dde.grad.hessian(u_vel, x, i=0, j=2)
-    def u_yt(x, u):
-        u_vel = u[:, 0:1]
-        return dde.grad.hessian(u_vel, x, i=1, j=2)
-    def u_xyt(x, u):
-        uxy= u_xy(x, u)
-        return dde.grad.jacobian(uxy, x, i=0, j=2)
-    def v_xy(x, u):
-        v_vel = u[:, 1:2]
-        return dde.grad.hessian(v_vel, x, i=0, j=1)
-    def v_xt(x, u):
-        v_vel = u[:, 1:2]
-        return dde.grad.hessian(v_vel, x, i=0, j=2)
-    def v_yt(x, u):
-        v_vel = u[:, 1:2]
-        return dde.grad.hessian(v_vel, x, i=1, j=2)
-    def v_xyt(x, u):
-        vxy = v_xy(x, u)
-        return dde.grad.jacobian(vxy, x, i=0, j=1)
-    def pde_u_xy(x, u):
-        pde_u = pde(x,u)
-        pde_u = pde_u[0]
-        return dde.grad.hessian(pde_u, x, i=0, j=1)
-    def pde_u_xt(x, u):
-        pde_u = pde(x,u)
-        pde_u = pde_u[0]
-        return dde.grad.hessian(pde_u, x, i=0, j=2)
-    def pde_u_yt(x, u):
-        pde_u = pde(x,u)
-        pde_u = pde_u[0]
-        return dde.grad.hessian(pde_u, x, i=1, j=2)
-    def pde_u_xyt(x,u):
-        pde_uxy = pde_u_xy(x,u)
-        return dde.grad.jacobian(pde_uxy,x,i=0,j=2)
-    def pde_v_xy(x,u):
-        pde_v = pde(x, u)
-        pde_v = pde_v[1]
-        return dde.grad.hessian(pde_v, x, i=0, j=1)
-    def pde_v_xt(x,u):
-        pde_v = pde(x, u)
-        pde_v = pde_v[1]
-        return dde.grad.hessian(pde_v, x, i=0, j=2)
-    def pde_v_yt(x,u):
-        pde_v = pde(x, u)
-        pde_v = pde_v[1]
-        return dde.grad.hessian(pde_v, x, i=1, j=2)
-    def pde_v_xyt(x,u):
-        pde_vxy = pde_v_xy(x, u)
-        return dde.grad.jacobian(pde_vxy, x, i=0, j=2)
 
     # This chunk of code describes the problem using dde structure. Varies depending on prescribed initial distribution.
     spacedomain = dde.geometry.Rectangle(xmin=[0, 0],xmax=[1,1]) # The x,y domain is a rectangle with corners 0,0 to 1,1.
@@ -196,88 +139,14 @@ def main(k=1, c=1, NumDomain=2000, NumResamples=100, method="Random", depth=3, i
     
     model = dde.Model(data, net)
 
-    # Initial Training before resampling
+    # The only training as no resampling
     print("Initial 15000 Adam steps")
     model.compile("adam", lr=0.001)
     model.train(epochs=15000, display_every=5000)
-    # print("Initial L-BFGS steps")
-    model.compile("L-BFGS")
-    model.train(display_every=1000)
+    model.compile("L-BFGS steps")
+    losshistory, train_state = model.train(display_every=5000)
 
     # Measuring error after initial phase. This information is not used by network to train.
-    pred = model.predict(xyt)
-    u_pred = pred[:,0]
-    v_pred = pred[:,1]
-
-    l2_error_u = dde.metrics.l2_relative_error(u_true, u_pred)
-    l2_error_v = dde.metrics.l2_relative_error(v_true, v_pred)
-
-    print("Finished initial steps. ")
-    print(f"l2_relative_error_u: {l2_error_u}")
-    print(f"l2_relative_error_v: {l2_error_v}")
-
-    for i in range(NumResamples): # Resampling loop begins. 100 is default, first run took ~4 hours...
-        X = geomtime.random_points(100000)
-        # --- Below, all the different info sources for resampling
-        if input1 == "residual" or input1 == "pde":
-            Y = np.abs(model.predict(X, operator=pde)).astype(np.float64)
-            Y = np.add(Y[0,:],Y[1,:])
-        elif input1 == "uv_xy":
-            Y1 = np.abs(model.predict(X, operator=u_xy)).astype(np.float64)
-            Y2 = np.abs(model.predict(X, operator=v_xy)).astype(np.float64)
-            Y = np.add(Y1,Y2)
-        elif input1 == "uv_xt":
-            Y1 = np.abs(model.predict(X, operator=u_xt)).astype(np.float64)
-            Y2 = np.abs(model.predict(X, operator=v_xt)).astype(np.float64)
-            Y = np.add(Y1,Y2)
-        elif input1 == "uv_yt":
-            Y1 = np.abs(model.predict(X, operator=u_yt)).astype(np.float64)
-            Y2 = np.abs(model.predict(X, operator=v_yt)).astype(np.float64)
-            Y = np.add(Y1,Y2)
-        elif input1 == "uv_xyt":
-            Y1 = np.abs(model.predict(X, operator=u_xyt)).astype(np.float64)
-            Y2 = np.abs(model.predict(X, operator=v_xyt)).astype(np.float64)
-            Y = np.add(Y1,Y2)
-        elif input1 == "pde_uvxy":
-            Y1 = np.abs(model.predict(X, operator=pde_u_xy))
-            Y2 = np.abs(model.predict(X, operator=pde_v_xy))
-            Y = np.add(Y1,Y2)
-        elif input1 == "pde_uvxt":
-            Y1 = np.abs(model.predict(X, operator=pde_u_xt))
-            Y2 = np.abs(model.predict(X, operator=pde_v_xt))
-            Y = np.add(Y1,Y2)
-        elif input1 == "pde_uvyt":
-            Y1 = np.abs(model.predict(X, operator=pde_u_yt))
-            Y2 = np.abs(model.predict(X, operator=pde_v_yt))
-            Y = np.add(Y1,Y2)
-        elif input1 == "pde_uvxyt":
-            Y1 = np.abs(model.predict(X, operator=pde_u_xyt))
-            Y2 = np.abs(model.predict(X, operator=pde_v_xyt))
-            Y = np.add(Y1,Y2)
-
-        err_eq = np.power(Y, k) / np.power(Y, k).mean() + c
-        err_eq_normalized = (err_eq / sum(err_eq))[:, 0]
-        X_ids = np.random.choice(a=len(X), size=NumDomain, replace=False, p=err_eq_normalized)
-        X_selected = X[X_ids]
-
-        data.replace_with_anchors(X_selected) # Change current points with selected X points
-
-        model.compile("adam", lr=0.001)
-        model.train(epochs=1000, display_every=5000)
-        model.compile("L-BFGS")
-        model.train(display_every=5000)
-
-        print("!\nFinished loop #{}\n".format(i+1))
-
-        pred = model.predict(xyt)
-        u_pred = pred[:,0]
-        v_pred = pred[:,1]
-
-        l2_error_u = dde.metrics.l2_relative_error(u_true, u_pred)
-        l2_error_v = dde.metrics.l2_relative_error(v_true, v_pred)
-
-        print(f"l2_relative_error_u: {l2_error_u}")
-        print(f"l2_relative_error_v: {l2_error_v}")
 
     y_pred = model.predict(xyt)
     u_pred = y_pred[:,0]
@@ -289,11 +158,11 @@ def main(k=1, c=1, NumDomain=2000, NumResamples=100, method="Random", depth=3, i
 
     time_taken = (time.time()-start_t)
     
-    # dde.saveplot(losshistory, train_state, issave=False, isplot=False, 
-    #              loss_fname=f"replacement_{input1}_D{depth}_{method}_k{k}c{c}_N{NumDomain}_L{NumResamples}_loss_info.dat", 
-    #              train_fname=f"replacement_{input1}_D{depth}_{method}_k{k}c{c}_N{NumDomain}_L{NumResamples}_finalpoints.dat", 
-    #              test_fname=f"replacement_{input1}_D{depth}_{method}_k{k}c{c}_N{NumDomain}_L{NumResamples}_finalypred.dat",
-    #              output_dir="../results/additional_info")
+    dde.saveplot(losshistory, train_state, issave=False, isplot=False, 
+                 loss_fname=f"be2d_fixed_{method}_D{depth}_k{k}c{c}_N{NumDomain}_L{NumResamples}_loss_info.dat", 
+                 train_fname=f"be2d_fixed_{method}_D{depth}_k{k}c{c}_N{NumDomain}_L{NumResamples}_finalpoints.dat", 
+                 test_fname=f"be2d_fixed_{method}_D{depth}_k{k}c{c}_N{NumDomain}_L{NumResamples}_finalypred.dat",
+                 output_dir="../results//be2d/loss_info")
     return error_final_u, error_final_v, time_taken
  
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -328,9 +197,9 @@ if __name__ == "__main__":
     # Directory to save to
     output_dir = "../results/be2d/error_time"
     # File name
-    error_u_fname = f"be2d_{input1}_D{depth}_{method}_k{k}c{c}_N{NumDomain}_L{NumResamples}_error_final_u.txt"
-    error_v_fname = f"be2d_{input1}_D{depth}_{method}_k{k}c{c}_N{NumDomain}_L{NumResamples}_error_final_v.txt"
-    time_taken_fname = f"be2d_{input1}_D{depth}_{method}_k{k}c{c}_N{NumDomain}_L{NumResamples}_time_taken.txt"
+    error_u_fname = f"be2d_fixed_{method}_D{depth}_k{k}c{c}_N{NumDomain}_L{NumResamples}_error_final_u.txt"
+    error_v_fname = f"be2d_fixed_{method}_D{depth}_k{k}c{c}_N{NumDomain}_L{NumResamples}_error_final_v.txt"
+    time_taken_fname = f"be2d_fixed_{method}_D{depth}_k{k}c{c}_N{NumDomain}_L{NumResamples}_time_taken.txt"
     
     # If results directory does not exist, this creates it
     if not os.path.exists(output_dir):
